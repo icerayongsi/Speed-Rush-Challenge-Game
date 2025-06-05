19376676701267a3d65cb612606914104f88362e
icerayongsi <icerayongsi@gmail.com>
2025-06-05 14:04:16 +0700

    version

--- src/components/ControlScreen.tsx
+++ src/components/ControlScreen.tsx
@@ -26,7 +26,7 @@ import {
 } from "lucide-react";
 import { socket, reconnectSocket } from "../socket";
 
-const packageVersion = "1.0.88";
+const packageVersion = "1.0.90";
 
 const ControlScreen: React.FC = () => {
   const [playerName, setPlayerName] = useState("");
